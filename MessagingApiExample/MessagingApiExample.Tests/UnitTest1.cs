f461c8f3fb39c7eb2f1b6e6b929c0f89249f5e27
ShassBeleth <shassbeleth@gmail.com>
2017-11-19 21:04:03 +0900

    Initial ASP

---
+++ MessagingApiExample/MessagingApiExample.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessagingApiExample.Tests {
+
+	[TestClass]
+	public class UnitTest1 {
+
+		[TestMethod]
+		public void TestMethod1() {
+
+		}
+
+	}
+
+}
---
+++ MessagingApiExample/MessagingApiExample/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace MessagingApiExample {
+
+	public class WebApiApplication : HttpApplication {
+
+		protected void Application_Start() => GlobalConfiguration.Configure( WebApiConfig.Register );
+
+	}
+
+}

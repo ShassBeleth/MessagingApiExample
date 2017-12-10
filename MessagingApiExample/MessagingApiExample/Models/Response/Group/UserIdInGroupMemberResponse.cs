42b38d9d837a98ff0486ef3477b7e97d6fa9b53f
ShassBeleth <shassbeleth@gmail.com>
2017-12-11 02:01:13 +0900

    グループメンバーのID取得完

---
+++ MessagingApiExample/MessagingApiExample/Models/Response/Group/UserIdInGroupMemberResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace MessagingApiExample.Models.Response.Group {
+
+	/// <summary>
+	/// グループメンバーのId取得Response
+	/// </summary>
+	public class UserIdInGroupMemberResponse {
+
+		/// <summary>
+		/// グループメンバーのユーザID
+		/// </summary>
+		public string[] memberIds;
+
+		/// <summary>
+		/// memberIdsに続きのユーザがある場合に返される
+		/// </summary>
+		public string next;
+
+	}
+
+}
--- MessagingApiExample/MessagingApiExample/Services/Group/GroupService.cs
+++ MessagingApiExample/MessagingApiExample/Services/Group/GroupService.cs
@@ -36,9 +36,9 @@ namespace MessagingApiExample.Services.Group {
 
 			string requestUrl = 
 				ConfigurationManager.AppSettings[ "BaseUrl" ] + 
-				ConfigurationManager.AppSettings[ "GroupUrlBefore" ] + 
+				ConfigurationManager.AppSettings[ "GroupUrl" ] + 
 				groupId + 
-				ConfigurationManager.AppSettings[ "GroupUrlAfter" ] + 
+				ConfigurationManager.AppSettings[ "GroupProfileUrl" ] + 
 				userId;
 
 			string resultAsString = null;
@@ -71,6 +71,64 @@ namespace MessagingApiExample.Services.Group {
 
 		}
 
-	}
+		// TODO 未確認
+		/// <summary>
+		/// グループメンバーのIdを取得する
+		/// </summary>
+		/// <param name="channelAccessToken">ChannelAccessToken</param>
+		/// <param name="groupId">グループID</param>
+		/// <param name="next">ユーザIDに続きがある場合に必要なキー</param>
+		/// <returns></returns>
+		public static async Task<UserIdInGroupMemberResponse> GetUserIdInGroupMember(
+			string channelAccessToken ,
+			string groupId ,
+			string next = null
+		) {
 
+			Trace.TraceInformation( "Start Get User Id In Group Member" );
+
+			HttpClient client = new HttpClient();
+			client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue( "application/json" ) );
+			client.DefaultRequestHeaders.Add( "Authorization" , "Bearer {" + channelAccessToken + "}" );
+
+			string requestUrl =
+				ConfigurationManager.AppSettings[ "BaseUrl" ] +
+				ConfigurationManager.AppSettings[ "GroupUrl" ] +
+				groupId +
+				ConfigurationManager.AppSettings[ "/members/ids" ] +
+				( next == null ? "" : ( "?start=" + next ) );
+			
+			string resultAsString = null;
+
+			try {
+
+				HttpResponseMessage response = await client.GetAsync( requestUrl );
+				resultAsString = await response.Content.ReadAsStringAsync();
+				response.Dispose();
+				client.Dispose();
+				Trace.TraceInformation( "Get User Id In Group Member Response is : " + resultAsString );
+				return JsonConvert.DeserializeObject<UserIdInGroupMemberResponse>( resultAsString );
+
+			}
+			catch( ArgumentNullException ) {
+				Trace.TraceError( "Get User Id In Group Member is Argument Null Exception" );
+				client.Dispose();
+				return null;
+			}
+			catch( HttpRequestException ) {
+				Trace.TraceError( "Get User Id In Group Member is Http Request Exception" );
+				client.Dispose();
+				return null;
+			}
+			catch( Exception ) {
+				Trace.TraceError( "Get User Id In Group Member is Unexpected Exception" );
+				client.Dispose();
+				return null;
+			}
+
+		}
+
+
+	}
+	
 }
